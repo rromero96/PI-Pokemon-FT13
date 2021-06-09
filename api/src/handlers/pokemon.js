22c98691b26918cac43b7dc87a8e9ed9aa329606
Roro <rodrigoignacio932@gmail.com>
2021-06-09 11:39:32 -0300

    push

--- api/src/handlers/pokemon.js
+++ api/src/handlers/pokemon.js
@@ -1,10 +1,11 @@
 const {Pokemon} = require('../db.js');
 const { v4: uuidv4 } = require('uuid');
+const axios = require('axios');
 
 
 async function getApiPokemon (req, res, next) {
-
-
+        const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon');
+        return res.json(pokemon.data);
 }
 
 async function getIdPokemon (req, res, next) { // hacer params y query
@@ -14,9 +15,8 @@ async function getIdPokemon (req, res, next) { // hacer params y query
 async function addPokemon (req, res, next) {
     const id = uuidv4();
     const pokemon = {...req.body, id};
-    if(!pokemon) {
-        return res.send({
-            error: 500,
+    if(!req.body.name) {
+        return res.send({      
             message: 'tenes que llenar los datos',
         });
     }
--- api/src/handlers/types.js
+++ api/src/handlers/types.js
@@ -1,7 +1,21 @@
 const {Tipo} = require('../db.js');
+const axios = require ('axios');
 
-async function getAddTypes (req, res, next) {
-
+async function getAddTypes (req, res) {
+    const dbTypes =await Tipo.findAll();
+    if(dbTypes.length === 0) {
+        try {
+            const types = await axios('https://pokeapi.co/api/v2/type');
+            for(let i in types.data.results){
+                await Tipo.create({name: types.data.results[i].name});
+                console.log(types.data.results[i].name)
+            }
+              return res.redirect('/types');
+         } catch(error) {
+            res.status(404).send('ERROR!')
+         }
+        }
+        return res.status(200).json(dbTypes);
 }
 
 module.exports = {
