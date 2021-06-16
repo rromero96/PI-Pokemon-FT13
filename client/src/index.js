ad1933f83408bfae7e1b8dcaa9f804ada9b5533b
Roro <rodrigoignacio932@gmail.com>
2021-06-15 23:04:46 -0300

    push

--- client/src/index.js
+++ client/src/index.js
@@ -6,17 +6,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
-/* import store from './store'; */
-//poner las etiquetas provider
-/* <Provider store={store}>
-</Provider>, */
+import { Provider } from "react-redux";
+import Store from "./store/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
+    <React.StrictMode>
+  <Provider store={Store}>
+  <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  </BrowserRouter>
+  </Provider>
+    </React.StrictMode> , 
   document.getElementById('root')
 );
 
