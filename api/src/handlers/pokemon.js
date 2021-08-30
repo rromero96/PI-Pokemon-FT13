f46c0f72df4e70df51fda2a2a113f98a2e8a1b37
Roro <rodrigoignacio932@gmail.com>
2021-08-30 12:57:02 -0300

    deploy

--- api/src/handlers/pokemon.js
+++ api/src/handlers/pokemon.js
@@ -142,7 +142,8 @@ async function getIdPokemon (req, res){
 
 async function addPokemon (req, res, next) {
     const id = uuidv4();
-    const pokemon = {...req.body, id};
+    var pokemon = {...req.body, id};
+    req.body.name = req.body.name ? req.body.name.toLowerCase() : req.body.name;
     if(!req.body.name || !req.body.type1 || req.body.height > 255|| req.body.weight > 255 || req.body.hp > 255 || req.body.attack > 255 || req.body.defense > 255 || req.body.speed > 255) {
         return res.status(500).send({      
             message: 'error',
